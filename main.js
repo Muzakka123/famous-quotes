f8d3e1c56e6c9beab38320f18789416b814f7251
Muzakka123 <159703338+Muzakka123@users.noreply.github.com>
2024-05-12 16:06:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var famousperson = "Albert Einstine";
+var famousquote = 'person who never made a mistake never tried anything new';
+var message = "".concat(famousperson, " once said , ").concat(famousquote);
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let famousperson :string="Albert Einstine";
+let famousquote : string = 'person who never made a mistake never tried anything new';
+let message:string = `${famousperson} once said , ${famousquote}`;
+console.log(message);
